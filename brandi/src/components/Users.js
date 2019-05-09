afd9c43a40fffc631b901a82bd7f4f9a5f0a2a9d
blubrandi <blubrandi0@gmail.com>
2019-05-09 16:39:06 -0600

    Added lifestyle method componentDidMount with axios request,  Wrapped export with requiresAuth on Users component.

--- brandi/src/components/Users.js
+++ brandi/src/components/Users.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import axios from 'axios'
 
+import requiresAuth from '../auth/requiresAuth'
+
 class Users extends React.Component {
     state = {
         users: []
@@ -22,6 +24,17 @@ class Users extends React.Component {
         )
 
     }
+
+    componentDidMount() {
+        const endpoint = '/users'
+
+        axios
+            .get(endpoint)
+            .then(res => {
+                this.setState({ users: res.data })
+            })
+            .catch(err => console.log(err))
+    }
 }
 
-export default Users
+export default requiresAuth(Users)
