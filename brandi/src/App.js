e374715818efb22dfdb6e958d4bd8810268b9c45
blubrandi <blubrandi0@gmail.com>
2019-05-09 17:49:13 -0600

    Added Register component.  Added Link and Route to register form in App.js

--- brandi/src/App.js
+++ brandi/src/App.js
@@ -3,6 +3,7 @@ import { Route, NavLink, withRouter } from 'react-router-dom'
 
 import Users from './components/Users'
 import Login from './components/auth/Login'
+import Register from './components/auth/Register'
 
 // import './App.css';
 
@@ -18,12 +19,14 @@ function App(props) {
       <header>
         <NavLink to='/login'> Login</NavLink> |
       <NavLink to='/users'> Users</NavLink> |
+      <NavLink to='/register'> Register</NavLink> |
         <div onClick={logout}> Logout</div>
       </header>
       <h1>Hey there from React app!</h1>
       <main>
         <Route path='/users' component={Users} />
         <Route path='/login' component={Login} />
+        <Route path='/register' component={Register} />
       </main>
     </div>
   );
---
+++ brandi/src/components/auth/Register.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import axios from 'axios';
+
+class Register extends React.Component {
+    state = {
+        username: '',
+        password: '',
+        department: ''
+    };
+
+    render() {
+        return (
+            <>
+                <h2>Register</h2>
+                <form onSubmit={this.submitForm}>
+                    <div>
+                        <label htmlFor="username" />
+                        <input
+                            id="username"
+                            onChange={this.handleChange}
+                            value={this.state.username}
+                            type="text"
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="password" />
+                        <input
+                            id="password"
+                            onChange={this.handleChange}
+                            value={this.state.password}
+                            type="password"
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="department" />
+                        <input
+                            id="department"
+                            onChange={this.handleChange}
+                            value={this.state.department}
+                            type="text"
+                        />
+                    </div>
+                    <div>
+                        <button type="submit">Login</button>
+                    </div>
+                </form>
+            </>
+        );
+    }
+
+    handleChange = event => {
+        const { id, value } = event.target;
+
+        this.setState({ [id]: value });
+    };
+
+    submitForm = event => {
+        event.preventDefault();
+        const endpoint = 'http://localhost:5100/api/auth/register';
+
+        axios
+            .post(endpoint, this.state)
+            .then(res => {
+                localStorage.setItem('jwt', res.data.token);
+                this.props.history.push('/users');
+            })
+            .catch(err => {
+                console.error('Registration Error', err);
+            });
+    };
+}
+
+export default Register;
