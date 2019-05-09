c6be6da9606bd484925f060ca973c684e0ccb659
blubrandi <blubrandi0@gmail.com>
2019-05-09 16:40:05 -0600

    Created auth dir in components dir

--- brandi/src/components/Users.js
+++ brandi/src/components/Users.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import axios from 'axios'
 
-import requiresAuth from '../auth/requiresAuth'
+import requiresAuth from './auth/requiresAuth'
 
 class Users extends React.Component {
     state = {
